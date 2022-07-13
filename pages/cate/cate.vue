038894827674dff9268a1e8275b2bcf1941c8e23
xya499 <302870488@qq.com>
2022-07-13 20:02:28 +0800

    完成分类页面

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,32 @@
 <template>
   <view>
-    cate
+    <view class="scroll-container">
+      <!-- 左侧滚动 -->
+      <scroll-view class="left-scroll-container" scroll-y="true" :style="{height: wh+'px'}">
+        <block v-for="(item,index) in cateList" :key="item.cat_id">
+          <view :class=" ['left-scroll-item', item.cat_id === active ? 'active' : '']"
+            @click="activeChanged(item.cat_id,index)">
+            {{item.cat_name}}
+          </view>
+        </block>
+
+      </scroll-view>
+      <!-- 右侧滚动 -->
+      <scroll-view class="right-scroll-container" scroll-y="true" :style="{height: wh+ 'px'}" :scroll-top='scrollTop'>
+        <!-- 二级列表 -->
+        <view class=" cateLevel2" v-for="item in cateLevel2" :key="item.cat_id">
+          <view class="cateLevel2-item">/{{item.cat_name}}/</view>
+          <!-- 三级列表 -->
+          <view class="cateLevel3">
+            <view class="cateLevel3-list" v-for="(item2) in item.children" :key="item2.cat_id"
+              @click="gotoGoodsList(item2)">
+              <image :src="item2.cat_icon" mode=""></image>
+              <view class="cateLevel3-item">{{item2.cat_name}}</view>
+            </view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +34,131 @@
   export default {
     data() {
       return {
-
+        // 窗口的可用高度 = 屏幕高度 - navigationBar高度 - tabBar 高度
+        wh: 0,
+        //分类数据列表
+        cateList: [],
+        //默认active在第一个
+        active: 0,
+        //二级分类列表
+        cateLevel2: [],
+        // 滚动条距离顶部的距离
+        scrollTop: 0
       };
+    },
+    onLoad() {
+      // 获取当前系统的信息
+      const sysInfo = uni.getSystemInfoSync()
+      // 为 wh 窗口可用高度动态赋值
+      this.wh = sysInfo.windowHeight
+
+      //获取分类数据列表
+      this.getCateList()
+
+    },
+    methods: {
+      //获取分类数据
+      async getCateList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/categories')
+        console.log(res);
+        if (res.meta.status !== 200) {
+          return uni.$showMsg()
+        }
+        this.cateList = res.message
+        //获取二级分类列表
+        this.cateLevel2 = res.message[0].children
+        //获取默认active值
+        this.active = res.message[0].cat_id
+
+        // console.log(this.active);
+      },
+
+      //动态添加active类
+      activeChanged(cat_id, index) {
+        //获取对应的cat_id
+        this.active = cat_id
+        //重新获取二级分类列表
+        this.cateLevel2 = this.cateList[index].children
+        //重置滚动条位置
+        this.scrollTop = this.scrollTop ? 0 : 1
+        // console.log(this.cateLevel3);
+      },
+      //跳转到商品列表页面
+      gotoGoodsList(item2) {
+        uni.navigateTo({
+          url: '/subpkg/goods_list/goods_list?cid=' + item2.cat_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .scroll-container {
+    display: flex;
 
+    .left-scroll-container {
+      width: 120px;
+
+      .left-scroll-item {
+        line-height: 60px;
+        background-color: #f7f7f7;
+        text-align: center;
+        font-size: 12px;
+
+        &.active {
+          background-color: #ffffff;
+          position: relative;
+
+          // 渲染激活项左侧的红色指示边线
+          &::before {
+            content: ' ';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #c00000;
+            position: absolute;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
+
+    .right-scroll-container {
+      .cateLevel2 {
+        .cateLevel2-item {
+          font-size: 12px;
+          font-weight: bold;
+          text-align: center;
+          padding: 15px 0;
+        }
+
+        .cateLevel3 {
+          display: flex;
+          flex-wrap: wrap;
+
+          .cateLevel3-list {
+            width: 33.33%;
+            margin-bottom: 10px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            image {
+              width: 60px;
+              height: 60px;
+            }
+
+            .cateLevel3-item {
+              font-size: 12px;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7,6 +7,48 @@ function makeMap(str, expectsLowerCase) {
   }
   return expectsLowerCase ? (val) => !!map[val.toLowerCase()] : (val) => !!map[val];
 }
+function normalizeClass(value) {
+  let res = "";
+  if (isString(value)) {
+    res = value;
+  } else if (isArray(value)) {
+    for (let i = 0; i < value.length; i++) {
+      const normalized = normalizeClass(value[i]);
+      if (normalized) {
+        res += normalized + " ";
+      }
+    }
+  } else if (isObject(value)) {
+    for (const name in value) {
+      if (value[name]) {
+        res += name + " ";
+      }
+    }
+  }
+  return res.trim();
+}
+const toDisplayString = (val) => {
+  return isString(val) ? val : val == null ? "" : isArray(val) || isObject(val) && (val.toString === objectToString || !isFunction(val.toString)) ? JSON.stringify(val, replacer, 2) : String(val);
+};
+const replacer = (_key, val) => {
+  if (val && val.__v_isRef) {
+    return replacer(_key, val.value);
+  } else if (isMap(val)) {
+    return {
+      [`Map(${val.size})`]: [...val.entries()].reduce((entries, [key, val2]) => {
+        entries[`${key} =>`] = val2;
+        return entries;
+      }, {})
+    };
+  } else if (isSet(val)) {
+    return {
+      [`Set(${val.size})`]: [...val.values()]
+    };
+  } else if (isObject(val) && !isArray(val) && !isPlainObject(val)) {
+    return String(val);
+  }
+  return val;
+};
 const EMPTY_OBJ = Object.freeze({});
 const EMPTY_ARR = Object.freeze([]);
 const NOOP = () => {
@@ -72,8 +114,8 @@ const def = (obj, key, value) => {
   });
 };
 const toNumber = (val) => {
-  const n = parseFloat(val);
-  return isNaN(n) ? val : n;
+  const n2 = parseFloat(val);
+  return isNaN(n2) ? val : n2;
 };
 const LOCALE_ZH_HANS = "zh-Hans";
 const LOCALE_ZH_HANT = "zh-Hant";
@@ -392,9 +434,9 @@ function assertType$1(value, type) {
   let valid;
   const expectedType = getType$1(type);
   if (isSimpleType$1(expectedType)) {
-    const t = typeof value;
-    valid = t === expectedType.toLowerCase();
-    if (!valid && t === "object") {
+    const t2 = typeof value;
+    valid = t2 === expectedType.toLowerCase();
+    if (!valid && t2 === "object") {
       valid = value instanceof type;
     }
   } else if (expectedType === "Object") {
@@ -875,7 +917,7 @@ const $off = defineSyncApi(API_OFF, (name, callback) => {
   }
   if (!isArray(name))
     name = [name];
-  name.forEach((n) => emitter.off(n, callback));
+  name.forEach((n2) => emitter.off(n2, callback));
 }, OffProtocol);
 const $emit = defineSyncApi(API_EMIT, (name, ...args) => {
   emitter.emit(name, ...args);
@@ -3109,9 +3151,9 @@ const PublicInstanceProxyHandlers = {
     }
     let normalizedProps;
     if (key[0] !== "$") {
-      const n = accessCache[key];
-      if (n !== void 0) {
-        switch (n) {
+      const n2 = accessCache[key];
+      if (n2 !== void 0) {
+        switch (n2) {
           case 1:
             return setupState[key];
           case 2:
@@ -3862,7 +3904,7 @@ function isSameType(a, b) {
 }
 function getTypeIndex(type, expectedTypes) {
   if (isArray(expectedTypes)) {
-    return expectedTypes.findIndex((t) => isSameType(t, type));
+    return expectedTypes.findIndex((t2) => isSameType(t2, type));
   } else if (isFunction(expectedTypes)) {
     return isSameType(expectedTypes, type) ? 0 : -1;
   }
@@ -3910,9 +3952,9 @@ function assertType(value, type) {
   let valid;
   const expectedType = getType(type);
   if (isSimpleType(expectedType)) {
-    const t = typeof value;
-    valid = t === expectedType.toLowerCase();
-    if (!valid && t === "object") {
+    const t2 = typeof value;
+    valid = t2 === expectedType.toLowerCase();
+    if (!valid && t2 === "object") {
       valid = value instanceof type;
     }
   } else if (expectedType === "Object") {
@@ -5130,6 +5172,8 @@ function vFor(source, renderItem) {
 }
 const o = (value, key) => vOn(value, key);
 const f = (source, renderItem) => vFor(source, renderItem);
+const n = (value) => normalizeClass(value);
+const t = (val) => toDisplayString(val);
 function createApp$1(rootComponent, rootProps = null) {
   rootComponent && (rootComponent.mpType = "app");
   return createVueApp(rootComponent, rootProps).use(plugin);
@@ -5939,4 +5983,6 @@ exports._export_sfc = _export_sfc;
 exports.createSSRApp = createSSRApp;
 exports.f = f;
 exports.index = index;
+exports.n = n;
 exports.o = o;
+exports.t = t;
