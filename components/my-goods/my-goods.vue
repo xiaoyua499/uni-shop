90e76bb4f04e2cd20daacff99abb87db88ce2f39
xya499 <302870488@qq.com>
2022-07-16 20:27:28 +0800

    完成商品列表页面

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+  <view>
+    <view class="goods-item">
+      <!-- 左侧图片 -->
+      <view class="goods-item-left">
+        <image class="goods-pic" :src="goods.goods_small_logo || defaultPic" mode=""></image>
+      </view>
+      <!-- 右侧信息区 -->
+      <view class="goods-item-right">
+        <!-- 商品标题 -->
+        <view class="goods-name">
+          {{goods.goods_name}}
+        </view>
+        <!-- 商品价格 -->
+        <view class="goods-info-box">
+          <view class="goods-price">
+            ￥{{goods.goods_price.toFixed(2)}}
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-goods",
+    //接收外部传来的数据
+    props: {
+      goods: {
+        type: Object,
+        default: {},
+      }
+    },
+    data() {
+      return {
+        //默认商品图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+  }
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+
+    .goods-item-left {
+      margin-right: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+  }
+</style>
--- unpackage/dist/dev/mp-weixin/subpkg/goods_list/goods_list.js
+++ unpackage/dist/dev/mp-weixin/subpkg/goods_list/goods_list.js
@@ -2,11 +2,83 @@
 var common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      queryObj: {
+        query: "",
+        cid: "",
+        pagenum: 1,
+        pagesize: 10
+      },
+      goodsList: [],
+      totaol: 0,
+      isloading: false
+    };
+  },
+  onLoad(options) {
+    this.queryObj.query = options.query || "";
+    this.queryObj.cid = options.cid || "";
+    this.getGoodsList();
+  },
+  methods: {
+    async getGoodsList(cb) {
+      console.log(this.queryObj);
+      this.isloading = true;
+      const {
+        data: res
+      } = await common_vendor.index.$http.get("/api/public/v1/goods/search", this.queryObj);
+      this.isloading = false;
+      cb && cb();
+      if (res.meta.status !== 200) {
+        return common_vendor.index.$showMsg();
+      }
+      this.goodsList = [...this.goodsList, ...res.message.goods];
+      this.totaol = res.message.totaol;
+      console.log(this.goodsList);
+    },
+    gotoDetail(value) {
+      common_vendor.index.navigateTo({
+        url: "/subpkg/goods_detail/goods_detail?goods_id=" + value.goods_id
+      });
+    }
+  },
+  onReachBottom() {
+    if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total)
+      return common_vendor.index.$showMsg("\u5DF2\u7ECF\u5230\u5E95\u5566\uFF01\uFF01\uFF01");
+    if (this.isloading)
+      return;
+    this.queryObj.pagenum += 1;
+    this.getGoodsList();
+  },
+  onPullDownRefresh() {
+    this.queryObj.pagenum = 1;
+    this.totaol = 0;
+    this.goodsList = [];
+    this.isloading = false;
+    this.getGoodsList(() => common_vendor.index.stopPullDownRefresh());
   }
 };
+if (!Array) {
+  const _easycom_my_goods2 = common_vendor.resolveComponent("my-goods");
+  const _component_v = common_vendor.resolveComponent("v");
+  (_easycom_my_goods2 + _component_v)();
+}
+const _easycom_my_goods = () => "../../components/my-goods/my-goods.js";
+if (!Math) {
+  _easycom_my_goods();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.f($data.goodsList, (item, index, i0) => {
+      return {
+        a: "e34b0caa-1-" + i0 + ",e34b0caa-0",
+        b: common_vendor.p({
+          goods: item
+        }),
+        c: index,
+        d: common_vendor.o(($event) => $options.gotoDetail(item))
+      };
+    })
+  };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/Desktop/\u6587\u4EF6/web/\u9879\u76EE/uni-shop/subpkg/goods_list/goods_list.vue"]]);
 wx.createPage(MiniProgramPage);
